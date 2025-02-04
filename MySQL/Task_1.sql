0a4b011e0eb3ad8c9280e3e153c2b25052f6fd58
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-02-04 23:25:53 +0200

    Update Task_1.sql

--- MySQL/Task_1.sql
+++ MySQL/Task_1.sql
@@ -1,1 +1,11 @@
+-- Basic Select ( Select All )
 
+select * from city;
+
+-- Basic Select ( Weather Observation Station 1 )
+
+select city , state from station;
+
+-- Basic Select ( Select By ID )
+
+select * from city where ID = 1661;
