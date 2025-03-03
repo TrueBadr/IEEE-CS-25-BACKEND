495d57f18e00d2d960c48698c582c3d4683ab891
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-03 14:53:15 +0200

    Update Task1.php

--- PHP/Task1/Assignment3/Task1.php
+++ PHP/Task1/Assignment3/Task1.php
@@ -1,20 +1,18 @@
+<?php $name = "IEEE Zagazig Courses" ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Elzero Courses">
-    <title>Welcome To Elzero Courses</title>
-  </head>
-  <body>
-    <?php
-    $Name = "Elzero Courses";
-    ?>
-    <h1><?php echo $Name; ?></h1>
-    <p>Here In <?php echo $Name; ?> We Provide Front-End And Back-End Courses</p>
-    <hr>
-    <div><?php echo $Name; ?> Is The What You Need.</div>
-    <footer>All Right Reserved To <?php echo $Name; ?></footer>
-  </body>
+    <title>Welcome To <?php echo $name?></title>
+</head>
+<body>
+<h1><?php echo $name?></h1>
+<p>Here In <?php echo $name?> We Provide Front-End And Back-End Courses</p>
+<hr>
+<div><?php echo $name?> Is The What You Need.</div>
+<footer>All Right Reserved To <?php echo $name?></footer>
+</body>
 </html>
