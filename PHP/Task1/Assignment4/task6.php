715ed2a001c12dfcb713ec3a3507d1c54bcb1edb
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-03 18:55:23 +0200

    Update task6.php

--- PHP/Task1/Assignment4/task6.php
+++ PHP/Task1/Assignment4/task6.php
@@ -2,8 +2,9 @@
 $a = "Elzero";
 $b = "Web";
 $c = "School";
- // $d = $a." ".$b." ".$c; // 1
- // $d=$a." ";$d.=$b." ";$d.=$c; // 2
+// $d = $a." ".$b." ".$c; // 1
+// $d=$a." ";$d.=$b." ";$d.=$c; // 2
 // $d = "$a  $b  $c"; // 3
+// $d = implode([$a , " " ,  $b , " "  , $c]);  //4 
 echo $d; // Elzero Web School
 ?>
