944f84ddd6b5fddac031c85de59935061b049e41
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-03 06:20:45 +0200

    Create task8.php

---
+++ PHP/Task1/Assignment2/task8.php
@@ -0,0 +1,35 @@
+<?php
+print_r(
+  [
+  "Frontend" => 
+              [
+                "0"=>"HTML",
+                "1"=>"CSS" ,
+                "JS"=> 
+                       [ 
+                          "VueJs"=>
+                                     [
+                                     "2"=>"v2", 
+                                     "3"=>"v3"
+                                     ]
+                          ,"0"=>"ReactJs", 
+                          "1"=>"Svelte"
+                       ]
+              ],
+  "Backend" => 
+             [
+              "0"=>"PHP", 
+              "1"=>"MySQL", 
+              "2"=>"Security"
+              ],
+ "0"=>"Git",
+ "1"=>"Githup",
+ "Testing"=>
+             [
+              "0"=>"Unit Testing", 
+              "1"=>"End to End"  ,
+              "2"=>"Integration"
+              ]
+  ]
+);
+?>
