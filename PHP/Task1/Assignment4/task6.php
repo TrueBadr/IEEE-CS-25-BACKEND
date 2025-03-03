4035df92e6cc378ad61b5e07bfa6bd11d58e5041
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-03 17:46:40 +0200

    Create task6.php

---
+++ PHP/Task1/Assignment4/task6.php
@@ -0,0 +1,9 @@
+<?php
+$a = "Elzero";
+$b = "Web";
+$c = "School";
+ // $d = $a." ".$b." ".$c; // 1
+ // $d=$a." ";$d.=$b." ";$d.=$c; // 2
+// $d = "$a  $b  $c"; // 3
+echo $d; // Elzero Web School
+?>
