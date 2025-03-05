6966328d4abeb0c9f3350058a605c1aad82398e1
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-05 06:32:06 +0200

    Update task1.php

--- PHP/Task2/Assignment1/task1.php
+++ PHP/Task2/Assignment1/task1.php
@@ -1,7 +1,8 @@
 <?php
-$a=100;
-$b=200;
-$c=100;
-if ($b>$a&&$a==$c&&($a+$c)==$b)echo "Yes";
+$a = 100;
+$b = 200;
+$c = 100;
+if($b > $a && $a===$c && ($a+$c)===$b)echo "Yes";
 else echo "No";
+
 ?>
