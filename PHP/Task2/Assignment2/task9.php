8967bb194d1976b051ec8f29a639ca42de7ef7e2
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-05 22:58:42 +0200

    Update task9.php

--- PHP/Task2/Assignment2/task9.php
+++ PHP/Task2/Assignment2/task9.php
@@ -1,1 +1,14 @@
+<?php
+$help_num = 3;
+$nums = [4, 5, 6, 1, 2, 3];
+$names = ["Ahmed", "Sayed", "Osama", "Mahmoud", "Gamal"];
 
+for($i=0;$i<sizeof($nums)-1;$i++)
+{
+    if($i==$help_num)
+    {
+        echo $names[$nums[$i]]."\n".$names[$nums[$i+1]];
+        break;
+    }
+}
+?>
