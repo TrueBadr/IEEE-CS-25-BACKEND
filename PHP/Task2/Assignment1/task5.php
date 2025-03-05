37d6cc9085ba9a69389034e233e647f071dbd3bc
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-05 06:56:32 +0200

    Create task5.php

---
+++ PHP/Task2/Assignment1/task5.php
@@ -0,0 +1,6 @@
+<?php
+$name = "Osama";
+$age = 40;
+$country = "Egypt";
+if ($age > 18 && gettype($name) === "string" && $country === "Egypt")echo "The Age Is Good To Go\nThe Name Is Good To Go\nThe Country Is Good To Go";
+?>
