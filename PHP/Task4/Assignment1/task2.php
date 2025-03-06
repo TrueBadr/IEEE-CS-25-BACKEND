746ea8af49e93779f7b95f9f32a52b4a06d25e29
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-06 23:20:43 +0200

    Create task2.php

---
+++ PHP/Task4/Assignment1/task2.php
@@ -0,0 +1,4 @@
+<?php
+echo "Size In Megabyte Is " . round(filesize("B:\Courses\IEEE Backend (2024-2025)\PHP\Elzero\Learn PHP 8 In Arabic 2022 - _082 - File System Function Part One 4 Functions(480P)(720P_HD).mp4")/(1024*1024), 2) . "\n";
+echo "Size In Kilobyte Is " . round(filesize("B:\Courses\IEEE Backend (2024-2025)\PHP\Elzero\Learn PHP 8 In Arabic 2022 - _082 - File System Function Part One 4 Functions(480P)(720P_HD).mp4")/1024, 2) . "\n";
+?>
