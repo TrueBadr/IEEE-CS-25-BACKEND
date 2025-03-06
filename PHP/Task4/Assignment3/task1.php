325cef8e7807ff55e7fb53ea2bcb23ab4d57bd4e
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-06 23:32:30 +0200

    Create task1.php

---
+++ PHP/Task4/Assignment3/task1.php
@@ -0,0 +1,6 @@
+<?php
+$end=time()+1209600;
+setcookie('site[layout]', 'boxed', $end, '/', 'localhost');
+setcookie('site[font]'  , 'Swat' , $end, '/', 'localhost');
+setcookie('site[color]' , 'blue' , $end, '/', 'localhost');
+?>
