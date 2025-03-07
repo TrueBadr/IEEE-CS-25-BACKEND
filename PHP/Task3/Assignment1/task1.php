a07cb48c277813c2fa35215f2d2e1792050bdabe
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-07 15:05:37 +0200

    Update task1.php

--- PHP/Task3/Assignment1/task1.php
+++ PHP/Task3/Assignment1/task1.php
@@ -4,7 +4,7 @@ $num_two = 2.5;
 $let_one = "o";
 $let_two = "z";
 $str = "El%er0";
-$str[(int)$num_two]=$let_two;
 $str[$num_one]=$let_one;
-echo $str;
+$str[(int)$num_two]=$let_two;
+echo $str; // Elzero
 ?>
