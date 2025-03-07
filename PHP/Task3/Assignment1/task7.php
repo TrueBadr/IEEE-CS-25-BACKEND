1294f0106894ea875d9952cbb278d5edb0b5445f
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-07 16:03:34 +0200

    Update task7.php

--- PHP/Task3/Assignment1/task7.php
+++ PHP/Task3/Assignment1/task7.php
@@ -1,8 +1,6 @@
 <?php
 $chars = ["E", 1, 2, "l", "z", "E", "R", "o"];
-if(gettype($chars[0]=="string"))echo strtoupper($chars[0]);
-for($i=1;$i<sizeof($chars);$i++)
-{
-    if(gettype($chars[$i])=='string')echo strtolower($chars[$i]);
-}
+$ans = implode($chars);
+$s = ["1" => null , "2"=> null];
+echo ucfirst(strtolower(strtr($ans,$s)));
 ?>
