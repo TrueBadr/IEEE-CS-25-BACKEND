984a994c8df047656a8d820ce6c66ad620fe1bc0
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-08 17:03:56 +0200

    Update task3.php

--- PHP/Task3/Assignment2/task3.php
+++ PHP/Task3/Assignment2/task3.php
@@ -1,12 +1,11 @@
 <?php
-$friends =
-    [
+$friends = [
     "Ahmed Gamal" => "AG",
     "Osama Mohamed" => "OM",
     "Mahmoud Gamal" => "MG",
     "Ahmed Samy" => "AS"
-    ];
-$result=array_flip($friends);
-$result=array_change_key_case($result,CASE_LOWER);
-print_r($result);
+];
+print_r(array_reverse(array_change_key_case(array_flip($friends)),1));
 ?>
+
+
