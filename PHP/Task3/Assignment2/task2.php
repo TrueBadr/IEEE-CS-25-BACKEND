69176f7da9187f888778a9cfec41ad170c6073ba
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-08 16:58:44 +0200

    Update task2.php

--- PHP/Task3/Assignment2/task2.php
+++ PHP/Task3/Assignment2/task2.php
@@ -1,7 +1,5 @@
 <?php
 $codes = ["H", "C", "J"];
 $means = ["HTML", "CSS", "JavaScript"];
-$Array = [];
-for( $i=0 ; $i<3;$i++)$Array[strtolower(($codes[$i]))] = $means[$i];
-print_r($Array);
+print_r(array_change_key_case(array_combine($codes,$means),0));
 ?>
