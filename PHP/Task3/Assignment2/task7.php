0d44986b590075f5fffbdbc19f9bf794e8025d2f
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-08 18:39:48 +0200

    Create task7.php

---
+++ PHP/Task3/Assignment2/task7.php
@@ -0,0 +1,14 @@
+<?php
+$names = ["Osama", "Ahmed", "Sayed", "Mahmoud", "Ali"];
+next($names);
+next($names);
+echo current($names) . "<br>"; // "Sayed"
+next($names);
+next($names);
+echo current($names) . "<br>"; // "Ali"
+reset($names);
+echo current($names) . "<br>"; // "Osama"
+end($names);
+prev($names);
+echo current($names) . "<br>"; // "Mahmoud"
+?>
