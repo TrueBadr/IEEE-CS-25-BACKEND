4e3d404d7f77fa15594070016d55299aee893ba6
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-09 14:44:13 +0200

    Update task3.php

--- PHP/Task3/Assignment3/task3.php
+++ PHP/Task3/Assignment3/task3.php
@@ -1,6 +1,8 @@
 <?php
-$num1 = 11.5;
-$num2 = 9.4898;
-$num3 = -7.5;
-echo floor($num1)."\n".round($num2,1)."\n".ceil($num3);
+$num1 = 11.5; // 11
+$num2 = 9.4898; // 9.5
+$num3 = -7.5; // -7
+echo floor($num1)."\n";
+echo round($num2,1)."\n";
+echo ceil($num3)."\n";
 ?>
