95a36ca43032c326dc2ab08823d591c89139d8bf
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-09 15:36:25 +0200

    Update task3.php

--- PHP/Task4/Assignment1/task3.php
+++ PHP/Task4/Assignment1/task3.php
@@ -1,8 +1,8 @@
 <?php
 $dir1="Programming";
-$dir2="Programming/PHP";
-if (!file_exists($dir1))mkdir($dir1, 0711);
-if (!file_exists($dir2))mkdir($dir2, 0711);
-if (file_exists($dir2)) {rmdir($dir2);echo "Directory $dir2 Removed\n";}
-if (file_exists($dir1)) {rmdir($dir1);echo "Directory $dir1 Removed\n";}
+$dir2=$dir1."/PHP";
+if (!file_exists($dir1)) {mkdir($dir1, 0711);}
+if (!file_exists($dir2)) {mkdir($dir2, 0711);}
+if ( file_exists($dir2)) {rmdir($dir2);echo "Directory $dir2 Removed\n";}
+if ( file_exists($dir1)) {rmdir($dir1);echo "Directory $dir1 Removed\n";}
 ?>
