fb785f4b9c21f8fadeeeba8023285c3b0c4fc746
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-10 15:29:46 +0200

    Update task1.php

--- PHP/Task4/Assignment2/task1.php
+++ PHP/Task4/Assignment2/task1.php
@@ -1,4 +1,5 @@
 <?php
 date_default_timezone_set('Asia/Riyadh');
-echo date_default_timezone_get() . "\n".date("D, d M y - h:i:s A")."\n".date("l, d F Y - h:i:s A") . "\n";
+echo date("D, d M y - h:i:s A")."\n";
+echo date("l, d F Y - h:i:s A")."\n";
 ?>
