53982cc7c2b7ad6c116e52f438474cbe477fdeb7
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-03-10 23:54:22 +0200

    Update task3.php

--- PHP/Task4/Assignment3/task3.php
+++ PHP/Task4/Assignment3/task3.php
@@ -1,4 +1,8 @@
 <?php
-setcookie('site[layout]', '', time() - 3600, '/', 'localhost');
-print_r($_COOKIE);
+setcookie("site[font]", 'Swat');
+setcookie("site[color]", 'blue');
+echo "<pre>";
+print_r($_COOKIE['site']);
+echo "</pre>";
+echo "Your Color Is ".$_COOKIE['site']['color']." And Your Font Is ".$_COOKIE['site']['font'];
 ?>
